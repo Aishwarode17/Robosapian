d705d9af5689a32dc2d01c913c8dd958d112ae6b
aishwarode17 <aishwarmalviya8@gmail.com>
2021-10-28 03:10:38 +0530

    fixed reload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav style={{display:'flex',justifyContent:'flex-end'}}>
-            <p className="f3 pa4 link underline dim black pointer"><a className="black f3" href="/">Reload</a></p>            
+            <p className="f3 pa4 link underline dim black pointer"><a className="black f3" href="https://aishwarode17.github.io/Robosapian">Reload</a></p>            
         </nav>
     )
 }
